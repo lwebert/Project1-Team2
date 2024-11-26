a5ff1610f84d7bf89e810c03ee0e881415d4a350
lwebert <lauren@weberts.org>
2024-11-25 20:00:03 -0600

    Lauren add label elements to input.html

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//Function to read local storage
+function readLocalStorage() {
+    let storage = JSON.parse(localStorage.getItem('')) //using local storage key name ""
+}
+
+//Function to store local storage
+
+
+//Redirect pages
+//index.html Submit button --> input.html
+// const IndexSubmitEl = 
+
+//input.html Submit button --> index.html
+
+//movieinfo.html Edit button --> input.html
+
+//movieinfo.html Home button --> index.html
+
+
+
+
+
+
+
+
+
