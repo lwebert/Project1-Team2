c310cfc53569f47a567d6ceb107885d44361b531
lwebert <lauren@weberts.org>
2024-11-30 18:38:58 -0600

    Fixed JS to go from index.html to movieinfo.html

--- movieinfo.js
+++ movieinfo.js
@@ -1,3 +1,18 @@
+//Gather & display movie info you just clicked on
 
+let movieInfo = readLocalStorageMovieInfo();
+movieInfo = movieInfo[movieInfo.length - 1];
+
+let headingTitle = document.querySelector("#TempInfoTitle");
+headingTitle.textContent = movieInfo.infoTitle;
+
+let RatingEl = document.querySelector("#ratingInfo")
+RatingEl.textContent = movieInfo.infoRating;
+
+let GenreEl = document.querySelector("#genreInfo")
+GenreEl.textContent = movieInfo.infoGenre;
+
+let CommentEl = document.querySelector("#commentInfo")
+CommentEl.textContent = movieInfo.infoComment;
 
 
